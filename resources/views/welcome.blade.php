78991df3f82f719f8780aba3a1e52874b60e0ba0
geanruca <geanruca@gmail.com>
2019-09-06 00:08:58 -0400

    tomo, para no enamorarme, me enamoro, para no tomar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,61 +10,9 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
     <body>
-        <div>
+        <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
@@ -78,16 +26,40 @@
                     @endauth
                 </div>
             @endif
+        <div class="container">
 
-                @forelse($contactos as $c)
-                    {{$c->nombre}}<br>
-                    {{$c->edad}}<br>
-                    {{$c->atendido}}<br>
-                    {{$c->email}}<br>
-                    {{$c->tema}}<br>
-                    {{$c->mensaje}}<br>
-                    <img src="{{$c->url_imagen}}" alt="Smiley face" height="42" width="42"><br>
-                    <a href="{{$c->url_imagen}}">Link Imagen</a> <br>
+            @forelse($contactos as $c)
+            <div class="row">
+                <div class="col-md-3">
+                        {{$c->nombre}}<br>
+                </div>
+                <div class="col-md-3">
+                        {{$c->edad}}<br>
+                </div>
+                <div class="col-md-3">
+                        {{$c->atendido}}<br>
+                </div>
+                <div class="col-md-3">
+                        {{$c->email}}<br>
+                </div>
+                <div class="col-md-3">
+                        {{$c->tema}}<br>
+                </div>
+                <div class="col-md-3">
+                        {{$c->mensaje}}<br>
+                </div>
+                <div class="col-md-3">
+                        <img src="{{$c->url_imagen}}" alt="Smiley face" height="42" width="42"><br>
+                        <a href="{{$c->url_imagen}}">Link Imagen</a> <br>
+                </div>
+            </div>
+        </div>           
+                    
+                    
+                    
+                    
+                    
+                    
                 @empty
                     <p>No hay resultados que mostrar</p>
                 @endforelse
